6e92b97169df91f51002da0ec5410850258736bc
Mitchell Lee <mitchl2@uw.edu>
2014-01-06 13:47:37 -0800

    Commented parts of the code, started working on checkbox form elements

--- js/controllers/elements_controller.js
+++ js/controllers/elements_controller.js
@@ -8,16 +8,27 @@ var GRID_Y = 10;
 ODKScan.ElementsController = Ember.ArrayController.extend({
 	isImageEditing: false,
 	imgSelect: null,
+	addBorder: false,
 	init: function() {
 		this._super();		
-		console.log("init controller");
+		
 		var controller = this;
-		$(document).ready(function() {
+		$(document).ready(function() {		
+			/* 
+				imgAreaSelect field is initialized here,
+				setting it as field allows the controller to 
+				access/modify the the selected image region
+				based on interaction from the user.
+			*/
 			var ias = $('#image_area img').imgAreaSelect({
 								instance: true,
 								handles: true
 							});				
 			controller.set('imgSelect', ias);		
+						
+			/*
+				All dialog windows are initialized below.
+			*/
 			
 			$("#box_dialog").dialog({
 				autoOpen: false,
@@ -45,34 +56,79 @@ ODKScan.ElementsController = Ember.ArrayController.extend({
 						$("#box_dialog").dialog("close");
 					}
 				}
+			});
+				
+			$("#checkbox_dialog").dialog({
+				autoOpen: false,
+				modal: true,
+				buttons: {
+					"Ok": function() {
+						console.log("making checkboxes...");
+						var $new_cb = $('<div/>').css({width: 100, height: 100});
+						
+						if (controller.get('addBorder')) {						
+							$new_cb.css({'border-width': $("#cb_border").val()});
+							// NOTE: hard-coded color and style
+							$new_cb.css({'border-color': "black"});
+							$new_cb.css({'border-style': "solid"});
+							
+							// reset the selected border option
+							controller.send('disableBorder');
+						} else {
+							$new_cb.addClass('dashed_border');
+						}
+																	
+						$new_cb.draggable(
+							{containment: 'parent', 
+							grid: [GRID_X, GRID_Y]
+							});
+						$new_cb.resizable({containment: 'parent', grid: [GRID_X, GRID_Y]});					
+						
+						// checkboxes are removed when double-clicked
+						$new_cb.dblclick(
+							function() {
+								this.remove();
+							}
+						);
+						
+						$("#scan_doc").append($new_cb);
+						$("#checkbox_dialog").dialog("close");
+					},
+					"Cancel": function() {
+						$("#checkbox_dialog").dialog("close");
+					}
+				}
 			});			
 		});
 	},
 	actions: {
-		createElement: function () {
-		  var length = this.get('shapeLength');
-		  if (!length.trim()) { return; }
-		  var new_div = $('<div/>').draggable().text("BLAH");
-		  new_div.css('border', '1px solid black');
-		  new_div.css('width', length);
-		  new_div.css('height', length);
-
-		  // Create the new Shape model
-		  var currShape = this.store.createRecord('element', {
-			type: "square",
-			sideLength: length,
-			perimeter: 4 * length,
-			thisShape: new_div
-		  });
-		  
-		  $('body').append(new_div);
-
-		  // Clear the shapeLength input box
-		  this.set('shapeLength', '');
-
-		  // Save the new model
-		  currShape.save();
-		  console.log("created element");
+		enableBorder: function() {
+			this.set('addBorder', true);
+			/*
+				There's an issue with adding an 'action' to a
+				radio button, after selecting an option you're
+				unable to change the selected button. 
+				
+				As a work-around to this issue I'm manually 
+				changing the values of the radio buttons once
+				an option is selected.
+			*/
+			$(".border_yes").prop('checked', true);
+			$(".border_no").prop('checked', false);
+		},
+		disableBorder: function() {
+			this.set('addBorder', false);
+			/*
+				There's an issue with adding an 'action' to a
+				radio button, after selecting an option you're
+				unable to change the selected button. 
+				
+				As a work-around to this issue I'm manually 
+				changing the values of the radio buttons once
+				an option is selected.
+			*/
+			$(".border_no").prop('checked', true);
+			$(".border_yes").prop('checked', false);
 		},
 		enableImageEdit: function() {
 			$("#prop_sidebar").hide("slow");
@@ -118,43 +174,50 @@ ODKScan.ElementsController = Ember.ArrayController.extend({
 			var ias = this.get('imgSelect');
 			var reg = ias.getSelection();
 			if (reg.width == 0 && reg.height == 0) {
+				// indicates that no region is currently selected
+				// do nothing
 				console.log("no region selected");
 			} else {
 				console.log("region selected!");
-					var $new_img = $("<img/>").attr('src', $("#loaded_image").attr('src'));
+				var $new_img = $("<img/>").attr('src', $("#loaded_image").attr('src'));
 
-					$new_img.css({
-						position: 'relative',
-						top: -reg.y1,
-						left: -reg.x1,
-					});	
-													
-					// Image is wrapped in a div to eliminate the overflow
-					// and only make the selected region visible.
-					// NOTE: default position is set to top-left of Scan doc.
-					var $img_div = $('<div/>').css({
-						position: 'absolute', 
-						overflow: 'hidden',
-						width: reg.width, 
-						height: reg.height,
-						top: 0, 
-						left: 0});
-					
-					// image is removed when double-clicked
-					$img_div.dblclick(
-						function() {
-							this.remove();
-						}
-					);								
-					$img_div.append($new_img);
-					$img_div.draggable({containment: 'parent', position: 'relative'});
-					
-					$("#scan_doc").append($img_div);								
+				$new_img.css({
+					position: 'relative',
+					top: -reg.y1,
+					left: -reg.x1,
+				});	
+												
+				// Image is wrapped in a div to eliminate the overflow
+				// and only make the selected region visible.
+				// NOTE: default position of selected image
+				// is set to top-left of Scan doc.
+				var $img_div = $('<div/>').css({
+					position: 'absolute', 
+					overflow: 'hidden',
+					width: reg.width, 
+					height: reg.height,
+					top: 0, 
+					left: 0});
+				
+				// image is removed when double-clicked
+				$img_div.dblclick(
+					function() {
+						this.remove();
+					}
+				);								
+				$img_div.append($new_img);
+				$img_div.draggable({containment: 'parent', position: 'relative'});
+				
+				$("#scan_doc").append($img_div);								
 			}
 		},
 		createBox: function() {
 			console.log("creating box");
 			$("#box_dialog").dialog("open");
+		},
+		createCheckbox: function() {
+			console.log("creating checkboxes");
+			$("#checkbox_dialog").dialog("open");
 		}
 	}
 });

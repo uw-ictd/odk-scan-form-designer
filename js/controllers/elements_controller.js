25fc480fc317a03e3cb823bb6193440afd7d2502
Mitchell Lee <mitchl2@uw.edu>
2014-01-12 22:03:02 -0800

    Implemented fill-in bubble option for field elements

--- js/controllers/elements_controller.js
+++ js/controllers/elements_controller.js
@@ -4,7 +4,259 @@
 
 var GRID_X = 10;
 var GRID_Y = 10;
-var CHECKBOX_SIZE = 30;
+
+var CHECKBOX_SMALL = 20;
+var CHECKBOX_MEDIUM = 35;
+var CHECKBOX_LARGE = 50;
+
+var BUBBLE_SMALL = 20;
+var BUBBLE_MEDIUM = 35;
+var BUBBLE_LARGE = 50;
+
+/*
+	Parent class => GridElement
+		- Contains code to create the grid of element.
+	
+		Child class => Checkbox. 
+			- Sets fields used by the GridElement class.
+		Child class => Bubble
+			- Sets fields used by the GridElement class.
+*/
+
+function GridField() {
+}
+
+GridField.prototype.constructGrid = function() {
+	console.log("making grid...");
+	// NOTE: initial width and height are aligned to the grid size
+	var $grid_div = $('<div/>').addClass(this.grid_class).addClass('field');
+	
+	// the new field will be placed at the top left of the Scan doc
+	$grid_div.css({top: 0, left: 0, position: 'absolute'});																	
+	$grid_div.draggable({containment: 'parent', grid: [GRID_X, GRID_Y]});			
+	
+	for (var i = 0; i < this.num_rows; i++) {	
+		var mT;
+		var mB;
+		
+		// special case: only one row
+		if (this.num_rows == 1) {
+			mT = this.margin_top;
+			mB = this.margin_bottom;
+		} else if (i == 0) { // first row
+			mT = this.margin_top;
+			mB = (this.vert_dy - this.element_height) / 2;
+		} else if (i < this.num_rows - 1) { // middle row
+			mT = (this.vert_dy - this.element_height) / 2;
+			mB = (this.vert_dy - this.element_height) / 2;
+		} else { // last row
+			mT = (this.vert_dy - this.element_height) / 2;
+			mB = this.margin_bottom;
+		}
+	
+		// special case: only one checkbox in the row
+		if (this.num_cols == 1) {
+			var $g_element = $('<div/>').addClass(this.ele_class);
+			$g_element.css({width: this.element_width, height: this.element_height});		
+				
+			$g_element.css({marginLeft: this.margin_left, 
+					marginTop: mT, 
+					marginBottom: mB, 
+					marginRight: this.margin_right});
+			$grid_div.append($g_element);
+		} else {												
+			for (var j = 0; j < this.num_cols; j++) {	
+				var $g_element = $('<div/>').addClass(this.ele_class);
+				$g_element.css({width: this.element_width, height: this.element_height});
+
+				if (j == 0) { // edge case, first checkbox
+					$g_element.css({marginLeft: this.margin_left, 
+							marginTop: mT, 
+							marginBottom: mB, 
+							marginRight: (this.horiz_dx - this.element_width) / 2});
+				} else if (j < this.num_cols - 1) {
+					$g_element.css({marginLeft: (this.horiz_dx - this.element_width) / 2, 
+							marginTop: mT, 
+							marginBottom: mB, 
+							marginRight: (this.horiz_dx - this.element_width) / 2});
+				} else { // edge case, last checkbox
+					$g_element.css({marginLeft: (this.horiz_dx - this.element_width) / 2, 
+							marginTop: mT, 
+							marginBottom: mB, 
+							marginRight: this.margin_right});
+				}
+				$grid_div.append($g_element);
+			}
+		}		
+		$grid_div.append($("<br>"));							
+	}
+	
+	// all user-defined properties will be stored in a JSON object 
+	$grid_div.data("prop", {});		
+	
+	// set field properties
+	var field_prop = $grid_div.data("prop");	
+	/* TODO: allow user to manipulate these properties */												
+	field_prop.type = this.type;
+	field_prop.name = this.name;	
+	field_prop.label = this.label;
+	
+	var cf = {};
+	// initialize classifier 
+	cf.classifier_height = this.element_height;
+	cf.classifier_width = this.element_width;						
+	cf.training_data_uri = this.data_uri;
+	cf.classification_map = {empty: false};
+	cf.default_classification = true;
+	cf.advanced = {flip_training_data : true};	
+	
+	var getFieldJSON = function() {
+		var f_info = {};
+		
+		f_info.type = field_prop.type;
+		f_info.name = field_prop.name;
+		f_info.label = field_prop.label;
+		f_info.classifier = cf;
+		
+		f_info.segments = [];
+
+		var seg = {};
+		seg.segment_x = $grid_div.position().left;
+		seg.segment_y = $grid_div.position().top;
+		seg.segment_width = $grid_div.outerWidth();
+		seg.segment_height = $grid_div.outerHeight();
+		seg.align_segment = false;
+		
+		// seg.items contains list of locations of all grid elements
+		seg.items = [];
+		
+		$grid_div.children(this.ele_class).each(function() {
+			var ele_loc = {}; // stores location of the grid element
+			
+			/* 	NOTE: The element location is given with
+				respect to its center. Also, position().left
+				and position().right do not take into account
+				the margins around the div, we have to add
+				horiz_offset to account for the margin.
+			*/								
+			var horiz_offset = parseInt($(this).css('margin-left'));
+			var vert_offset = parseInt($(this).css('margin-top'));
+			
+			// we use outerWidth() and outerHeight() because they take borders into account
+			ele_loc.item_x = horiz_offset + $(this).position().left + ($(this).outerWidth() / 2);
+			ele_loc.item_y = vert_offset + $(this).position().top + ($(this).outerHeight() / 2);
+			
+			seg.items.push(ele_loc);
+		});
+		
+		f_info.segments.push(seg);
+		return f_info;
+	}
+	
+	$grid_div.data("getFieldJSON", getFieldJSON);
+	
+	// checkboxes are removed when double-clicked
+	$grid_div.dblclick(
+		function() {
+			this.remove();
+		}
+	);
+	
+	$("#scan_doc").append($grid_div);
+};
+
+function CheckboxField() {
+	// Set all checkbox attributes
+	
+	// set the grid class
+	this.grid_class = 'cb_div';
+	
+	// set the class of the grid elements
+	this.ele_class = 'c_box';
+	
+	// TODO: find out how these values should be set
+	this.type = 'int';
+	this.name = "square_checkboxes";	
+	this.label = "square_checkboxes";		
+	this.data_uri = "checkboxes";
+	
+	// checkbox size
+	this.element_width = ($("#cb_size").val() == 'small') ? CHECKBOX_SMALL : 
+						($("#cb_size").val() == 'medium') ? CHECKBOX_MEDIUM : CHECKBOX_LARGE;
+	this.element_height = ($("#cb_size").val() == 'small') ? CHECKBOX_SMALL : 
+						($("#cb_size").val() == 'medium') ? CHECKBOX_MEDIUM : CHECKBOX_LARGE;
+	
+	// the horizontal spacing between the centers of checkboxes
+	this.horiz_dx = parseInt($("#cb_horiz_dx").val());
+	
+	// the vetical spacing between the centers of checkboxes
+	this.vert_dy = parseInt($("#cb_vert_dy").val());
+	
+	// margin values
+	this.margin_top = parseInt($("#cb_margin_top").val());
+	this.margin_bottom = parseInt($("#cb_margin_bottom").val());
+	this.margin_left = parseInt($("#cb_margin_left").val());
+	this.margin_right = parseInt($("#cb_margin_right").val());
+	
+	// number of rows
+	this.num_rows = $("#num_row_cb").val();
+	
+	// number of columns
+	this.num_cols = $("#num_col_cb").val();
+}
+
+// inherit GridField
+CheckboxField.prototype = new GridField();
+
+// make the constructor point to the CheckboxField class
+CheckboxField.prototype.constructor = CheckboxField;
+
+function BubbleField() {
+	// Set all bubble attributes
+	
+	// set the grid class
+	this.grid_class = 'bubble_div';
+	
+	// set the class of the grid elements
+	this.ele_class = ($("#bubb_size").val() == 'small') ? 'bubble_small' : 
+						($("#bubb_size").val() == 'medium') ? 'bubble_med' : 'bubble_large';
+	
+	// TODO: find out what these values should actually be
+	this.type = 'int';
+	this.name = "circle_bubbles";	
+	this.label = "circle_bubbles";		
+	this.data_uri = "bubbles";
+	
+	// bubble size
+	this.element_width = ($("#bubb_size").val() == 'small') ? BUBBLE_SMALL : 
+						($("#bubb_size").val() == 'medium') ? BUBBLE_MEDIUM : BUBBLE_LARGE;
+	this.element_height = ($("#bubb_size").val() == 'small') ? BUBBLE_SMALL : 
+						($("#bubb_size").val() == 'medium') ? BUBBLE_MEDIUM : BUBBLE_LARGE;
+	
+	// the horizontal spacing between the centers of bubbles
+	this.horiz_dx = parseInt($("#bubble_horiz_dx").val());
+	
+	// the vetical spacing between the centers of bubbles
+	this.vert_dy = parseInt($("#bubble_vert_dy").val());
+	
+	// margin values
+	this.margin_top = parseInt($("#bubble_margin_top").val());
+	this.margin_bottom = parseInt($("#bubble_margin_bottom").val());
+	this.margin_left = parseInt($("#bubble_margin_left").val());
+	this.margin_right = parseInt($("#bubble_margin_right").val());
+	
+	// number of rows
+	this.num_rows = $("#num_row_bubbles").val();
+	
+	// number of columns
+	this.num_cols = $("#num_col_bubbles").val();
+}
+
+// inherit GridField
+BubbleField.prototype = new GridField();
+
+// make the constructor point to the BubbleField class
+BubbleField.prototype.constructor = BubbleField;
 
 ODKScan.ElementsController = Ember.ArrayController.extend({
 	isImageEditing: false,
@@ -120,158 +372,32 @@ ODKScan.ElementsController = Ember.ArrayController.extend({
 				buttons: {
 					"Ok": function() {
 						console.log("making checkboxes...");
-						// NOTE: initial width and height are aligned to the grid size
-						var $cb_div = $('<div/>').addClass('cb_div').addClass('field');
 						
-						// the new field will be placed at the top left of the Scan doc
-						$cb_div.css({top: 0, left: 0, position: 'absolute'});																		
-						$cb_div.draggable({containment: 'parent', grid: [GRID_X, GRID_Y]});				
+						var cbField = new CheckboxField();
+						cbField.constructGrid();						
 						
-						// the horizontal spacing between the centers of checkboxes
-						var horiz_dx = parseInt($("#cb_horiz_dx").val());
-						
-						// the vetical spacing between the centers of checkboxes
-						var vert_dx = parseInt($("#cb_vert_dy").val());
-						
-						// margin values
-						var MARGIN_TOP = parseInt($("#cb_margin_top").val());
-						var MARGIN_BOTTOM = parseInt($("#cb_margin_bottom").val());
-						var MARGIN_LEFT = parseInt($("#cb_margin_left").val());
-						var MARGIN_RIGHT = parseInt($("#cb_margin_right").val());
-						
-						for (var i = 0; i < $("#vert_num_cb").val(); i++) {	
-							var mT;
-							var mB;
-							
-							// special case: only one row
-							if ($("#vert_num_cb").val() == 1) {
-								mT = MARGIN_TOP;
-								mB = MARGIN_BOTTOM;
-							} else if (i == 0) { // first row
-								mT = MARGIN_TOP;
-								mB = (vert_dx - CHECKBOX_SIZE) / 2;
-							} else if (i < $("#vert_num_cb").val() - 1) { // middle row
-								mT = (vert_dx - CHECKBOX_SIZE) / 2;
-								mB = (vert_dx - CHECKBOX_SIZE) / 2;
-							} else { // last row
-								mT = (vert_dx - CHECKBOX_SIZE) / 2;
-								mB = MARGIN_BOTTOM;
-							}
-						
-							// special case: only one checkbox in the row
-							if ($("#horiz_num_cb").val() == 1) {
-								var $cb = $('<div/>').addClass('c_box');
-								$cb.css({width: CHECKBOX_SIZE, height: CHECKBOX_SIZE});		
-									
-								$cb.css({marginLeft: MARGIN_LEFT, 
-										marginTop: mT, 
-										marginBottom: mB, 
-										marginRight: MARGIN_RIGHT});
-								$cb_div.append($cb);
-							} else {												
-								for (var j = 0; j < $("#horiz_num_cb").val(); j++) {	
-									var $cb = $('<div/>').addClass('c_box');
-									$cb.css({width: CHECKBOX_SIZE, height: CHECKBOX_SIZE});
-
-									// TODO: remove hardcoded margin values
-									if (j == 0) { // edge case, first checkbox
-										$cb.css({marginLeft: MARGIN_LEFT, 
-												marginTop: mT, 
-												marginBottom: mB, 
-												marginRight: (horiz_dx - CHECKBOX_SIZE) / 2});
-									} else if (j < $("#horiz_num_cb").val() - 1) {
-										$cb.css({marginLeft: (horiz_dx - CHECKBOX_SIZE) / 2, 
-												marginTop: mT, 
-												marginBottom: mB, 
-												marginRight: (horiz_dx - CHECKBOX_SIZE) / 2});
-									} else { // edge case, last checkbox
-										$cb.css({marginLeft: (horiz_dx - CHECKBOX_SIZE) / 2, 
-												marginTop: mT, 
-												marginBottom: mB, 
-												marginRight: MARGIN_RIGHT});
-									}
-									$cb_div.append($cb);
-								}
-							}		
-							$cb_div.append($("<br>"));							
-						}
-						
-						// all user-defined properties will be stored in a JSON object 
-						$cb_div.data("prop", {});		
-						
-						// set field properties
-						var field_prop = $cb_div.data("prop");	
-						/* TODO: allow user to manipulate these properties */												
-						field_prop.type = "int";
-						field_prop.name = "square_checkboxes";	
-						field_prop.label = "square_checkboxes";	
-						
-						var cf = {};
-						// initialize classifier 
-						cf.classifier_height = CHECKBOX_SIZE;
-						cf.classifier_width = CHECKBOX_SIZE;						
-						cf.training_data_uri = "checkboxes";
-						cf.classification_map = {empty: false};
-						cf.default_classification = true;
-						cf.advanced = {flip_training_data : true};	
-						
-						var getFieldJSON = function() {
-							var f_info = {};
-							
-							f_info.type = field_prop.type;
-							f_info.name = field_prop.name;
-							f_info.label = field_prop.label;
-							f_info.classifier = cf;
-							
-							f_info.segments = [];
-				
-							var seg = {};
-							seg.segment_x = $cb_div.position().left;
-							seg.segment_y = $cb_div.position().top;
-							seg.segment_width = $cb_div.outerWidth();
-							seg.segment_height = $cb_div.outerHeight();
-							seg.align_segment = false;
-							
-							// seg.items contains list of locations of all checkboxes
-							seg.items = [];
-							
-							$cb_div.children('.c_box').each(function() {
-								var cb_loc = {}; // stores location of the checkbox
-								
-								/* 	NOTE: The checkbox location is given with
-									respect to its center. Also, position().left
-									and position().right do not take into account
-									the margins around the div, we have to add
-									horiz_offset to account for the margin.
-								*/								
-								var horiz_offset = parseInt($(this).css('margin-left'));
-								var vert_offset = parseInt($(this).css('margin-top'));
-								
-								// we use outerWidth() and outerHeight() because they take borders into account
-								cb_loc.item_x = horiz_offset + $(this).position().left + ($(this).outerWidth() / 2);
-								cb_loc.item_y = vert_offset + $(this).position().top + ($(this).outerHeight() / 2);
-								
-								seg.items.push(cb_loc);
-							});
-							
-							f_info.segments.push(seg);
-							return f_info;
-						}
-						
-						$cb_div.data("getFieldJSON", getFieldJSON);
-						
-						// checkboxes are removed when double-clicked
-						$cb_div.dblclick(
-							function() {
-								this.remove();
-							}
-						);
-						
-						$("#scan_doc").append($cb_div);
 						$("#checkbox_dialog").dialog("close");
 					},
 					"Cancel": function() {
 						$("#checkbox_dialog").dialog("close");
+					}
+				}
+			});			
+			
+			$("#bubble_dialog").dialog({
+				autoOpen: false,
+				modal: true,
+				buttons: {
+					"Ok": function() {
+						console.log("making fill-in bubbles...");
+						
+						var bubbField = new BubbleField();
+						bubbField.constructGrid();						
+						
+						$("#bubble_dialog").dialog("close");
+					},
+					"Cancel": function() {
+						$("#bubble_dialog").dialog("close");
 					}
 				}
 			});			
@@ -367,6 +493,10 @@ ODKScan.ElementsController = Ember.ArrayController.extend({
 		createCheckbox: function() {
 			console.log("creating checkboxes");
 			$("#checkbox_dialog").dialog("open");
+		},
+		createBubbles: function() {
+			console.log("creating fill-in bubbles");
+			$("#bubble_dialog").dialog("open");
 		},
 		saveJSON: function() {
 			console.log("creating JSON");

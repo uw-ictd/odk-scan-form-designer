2670dfdafcbecef30acc6425cd140cc994cbc338
Mitchell Lee <mitchl2@uw.edu>
2014-06-12 11:24:19 -0700

    Added top-level readme file.

--- js/application.js
+++ js/application.js
@@ -1,4 +1,2 @@
 // creates an instance of Ember.Application, adds it to the scope of the browser's JavaScript environment
 window.ODKScan = Ember.Application.create();
-
-ODKScan.ApplicationAdapter = DS.FixtureAdapter.extend();
